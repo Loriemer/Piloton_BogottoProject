bc1c721fa8e892bc5480251546cc0d4d11417c19
asus <asus@LAPTOP-5RR1SHUE>
2023-10-23 01:43:29 +0800

    Add project files.

---
+++ Piloton_BogottoProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Piloton_BogottoProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int firstItemPrice;
+            int secondItemPrice;
+            double discountedPrice;
+            double totalPrice;
+
+            Console.Write("Enter the price of the first item: ");
+            firstItemPrice = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the price of the second item:  ");
+            secondItemPrice = int.Parse(Console.ReadLine());
+
+            if (firstItemPrice < secondItemPrice)
+            {
+                discountedPrice = firstItemPrice / 2;
+                totalPrice = discountedPrice + secondItemPrice;
+            }
+            else
+            {
+                discountedPrice = secondItemPrice / 2;
+                totalPrice = discountedPrice + firstItemPrice;
+            }
+            Console.WriteLine($"The total amount of customer owes: {totalPrice}");
+        }
+    }
+}
